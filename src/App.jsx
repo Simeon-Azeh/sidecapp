92c1ff377efabfd08e3d107a88f1a201523f3e1e
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-07-21 13:56:21 +0200

    onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter ,Routes, Route, useLocation } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Login from './pages/Authentication/Login'
 import Register from './pages/Authentication/Register';
+import Onboarding from './pages/Authentication/Onbarding';
 
 
 function ScrollToTop() {
@@ -28,6 +29,7 @@ function App() {
        <Route path="*" element={<Landing/>} />
        <Route path="/auth/login" element={<Login/>} />
        <Route path="/auth/register" element={<Register/>} />
+       <Route path="/auth/onboarding" element={<Onboarding/>} />
      </Routes>
      </BrowserRouter>
     </>
