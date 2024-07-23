42ee749b157908f122f7b8c1620cd7f4487de482
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-07-23 09:54:33 +0200

    discover page started

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,15 +6,38 @@ import { RiArchiveDrawerFill } from "react-icons/ri";
 import { IoMdChatbubbles } from "react-icons/io";
 import { MdOutlineDatasetLinked } from "react-icons/md";
 import Navbar from '../components/Navbar';
-import { FaBook, FaChalkboardTeacher, FaCertificate } from 'react-icons/fa';
-import Card from '../components/Dashboard/Card';
 import ProfileContainer from '../components/Dashboard/ProfileContainer';
 import ResponsiveTable from '../components/Dashboard/ResponsiveTable';
 import CourseList from '../components/Courses/CourseList';
 import { Link } from 'react-router-dom';
 import GreetingCard from '../components/Discover/GreetingCard';
+import { Calendar, theme } from 'antd';
+import UpcomingToday from '../components/Discover/UpcomingToday';
+const onPanelChange = (value, mode) => {
+  console.log(value.format('YYYY-MM-DD'), mode);
+};
 
 function Discover() {
+    const { token } = theme.useToken();
+    const wrapperStyle = {
+      width: 300,
+      border: `1px solid ${token.colorBorderSecondary}`,
+      borderRadius: token.borderRadiusLG,
+    };
+    const sampleEnrolledUsers = [
+        { image: 'https://via.placeholder.com/40' },
+        { image: 'https://via.placeholder.com/40' },
+        { image: 'https://via.placeholder.com/40' },
+        { image: 'https://via.placeholder.com/40' },
+      ];
+
+      const sampleChapters = [
+        'Introduction',
+        'Chapter 1: Getting Started',
+        'Chapter 2: Basics',
+        'Chapter 3: Advanced Topics',
+        'Chapter 4: Conclusion',
+      ];
   return (
     <div className="flex h-screen">
         <div className='z-40'>
@@ -101,9 +124,20 @@ function Discover() {
          </div>
             </div>
             <div className='w-full px-7 mt-5 md:mt-0 md:px-0 md:w-[30%]'>
-            <div>
-                <ProfileContainer />
-            </div>
+            <div className='font-poppins mx-auto' style={wrapperStyle}>
+      <Calendar fullscreen={false} onPanelChange={onPanelChange}    />
+    </div>
+    <div className="mt-5 ">
+      <UpcomingToday 
+        mainImage="https://img.freepik.com/free-photo/flat-lay-paper-hand-holding-heart-with-copy-space_23-2148635149.jpg?t=st=1721720922~exp=1721724522~hmac=08b6d0046b470c6f26e910d3686e1ddbd00d15bdce514cb954c4e6de2fe3b342&w=996"
+        title="Empathy & Communication: neccesity for team dynamics"
+        description="This is a sample description of the course."
+        enrolledUsers={sampleEnrolledUsers}
+        completedChapters={2}
+        totalChapters={5}
+        chapters={sampleChapters}
+      />
+    </div>
             </div>
       
         </div>
